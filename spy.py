88350151577f819f5c5d50bfb61344e3147ca4dd
Abhijith1311 <65818243+Abhijith1311@users.noreply.github.com>
2021-11-30 23:38:06 +0530

    Add files via upload

---
+++ spy.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+st.title("Hello")
+df=pd.read_csv('original.csv')
+df.head()
+gender=st.radio('Select The Gender',('M','F'))
+rank=st
+rank=st.number_input('Enter Your Rank')
+caste=st.radio('Select The caste',('OC','BC_B'))
+branch_code=st.radio('Select your Required branch',('CSE','IT'))
+
+
+if branch_code=='None':
+	val=df[df['rank']>=rank]
+	val_x=val[val['gender']==gender]
+	val_y=val_x[val_x['caste']==caste]
+		
+	temp=val_y
+else:
+	val=df[df['rank']>=rank]
+	val_x=val[val['gender']==gender]
+	val_y=val_x[val_x['caste']==caste]
+	val_z=val_y[val_y['branch_code']==branch_code]
+		
+	temp=val_z
+y=temp.sort_values(by='rank', ascending = True)
+z=y.drop_duplicates(subset = ["college","branch_code"],keep='last')
+z.sort_values(by=['rank'],inplace=True)
+st.write(z.head())
